2b556e9ea89b49ed271bc66090bbe96f4a017144
m abiyyu arkan <abiyyuarkan20@gmail.com>
2026-02-09 13:02:15 +0700

    membuat website sohor coffee

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika humbergur menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klik diluar sidebar utk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
